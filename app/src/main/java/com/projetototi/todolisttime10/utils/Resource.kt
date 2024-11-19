f0572962f3d9091db051a0a196e942f6b14083c6
rkregna <roykeane@gmail.com>
2024-11-19 18:57:50 -0300

    add arquivo dao util task resource

---
+++ app/src/main/java/com/projetototi/todolisttime10/utils/Resource.kt
@@ -0,0 +1,10 @@
+package com.projetototi.todolisttime10.utils
+
+import com.projetototi.todolisttime10.utils.Status
+
+sealed class Resource<T>(val status: Status, val data: T? = null, val message : String? = null) {
+
+    class Success<T>(message: String, data: T? = null) : Resource<T>(Status.SUCCESS, data, message)
+    class Error<T>(message: String, data: T? = null) : Resource<T>(Status.ERROR, data, message)
+    class Loading<T> : Resource<T>(Status.LOADING)
+}
---
+++ app/src/main/java/com/projetototi/todolisttime10/viewmodels/TaskViewModel.kt
@@ -0,0 +1,47 @@
+
+// TaskViewModel.kt
+package com.projetototi.todolisttime10.viewmodels
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import com.projetototi.todolisttime10.models.Task
+import com.projetototi.todolisttime10.repository.TaskRepository
+
+class TaskViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val taskRepository = TaskRepository(application)
+    val taskStateFlow get() =  taskRepository.taskStateFlow
+    val statusLiveData get() =  taskRepository.statusLiveData
+    val sortByLiveData get() =  taskRepository.sortByLiveData
+
+    fun setSortBy(sort:Pair<String,Boolean>){
+        taskRepository.setSortBy(sort)
+    }
+
+    fun getTaskList(isAsc : Boolean, sortByName:String) {
+        taskRepository.getTaskList(isAsc, sortByName)
+    }
+
+    fun insertTask(task: Task){
+        taskRepository.insertTask(task)
+    }
+
+    fun deleteTask(task: Task) {
+        taskRepository.deleteTask(task)
+    }
+
+    fun deleteTaskUsingId(taskId: String){
+        taskRepository.deleteTaskUsingId(taskId)
+    }
+
+    fun updateTask(task: Task) {
+        taskRepository.updateTask(task)
+    }
+
+    fun updateTaskPaticularField(taskId: String,title:String,description:String) {
+        taskRepository.updateTaskPaticularField(taskId, title, description)
+    }
+    fun searchTaskList(query: String){
+        taskRepository.searchTaskList(query)
+    }
+}
